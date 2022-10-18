d58199637761cdafd0e450b71a846c577a6f91db
aximas <northdakota778@gmail.com>
2022-10-18 23:53:42 +0500

    add: click event tests, render app test, test className

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test app', () => {
+    test('render app', () => {
+        render(<App />);
+        const linkElement = screen.getByText(/hello from react/i);
+        const button = screen.getByRole('button');
+        const input = screen.getByPlaceholderText(/Type here .../i);
+
+        expect(linkElement).toBeInTheDocument();
+        expect(button).toMatchSnapshot();
+        expect(input).toBeInTheDocument();
+    });
+
+    test('test with classname', async () => {
+        render(<App />);
+        screen.debug();
+        const data = await screen.findByText(/data/i);
+        expect(data).toBeInTheDocument();
+        expect(data).toHaveClass('accent');
+        screen.debug();
+    });
+
+    test('test click event', () => {
+        render(<App />);
+        const btn = screen.getByTestId('toggle-btn');
+
+        expect(screen.queryByTestId('list')).toBeNull();
+
+        fireEvent.click(btn);
+        expect(screen.queryByTestId('list')).toBeInTheDocument();
+
+        fireEvent.click(btn);
+        expect(screen.queryByTestId('list')).toBeNull();
+    });
+
+    test('test input element', () => {
+        render(<App />);
+        const input = screen.getByPlaceholderText(/Type here .../i);
+        const value = 'New value';
+
+        expect(screen.queryByTestId('value-element')).toHaveTextContent('');
+
+        // Artifical event
+        // fireEvent.input(input, {
+        //     target: {value}
+        // });
+
+        // Real user event
+        userEvent.type(input, value);
+        expect(screen.queryByTestId('value-element')).toHaveTextContent(value);
+
+    });
 });
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState('');
+    const [toggle, setToggle] = useState(false);
+    const [value, setValue] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setData('Current Data');
+        }, 100);
+    }, []);
+
+    const handleClick = () => setToggle(prevState => !prevState);
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
+    return (
+        <div className="App">
+            <header className="App-header">
+                {data && <div className='accent'>data</div>}
+                {toggle ? (<ul data-testid='list' className='list'>
+                    <li>Ball</li>
+                    <li>Doll</li>
+                    <li>Cat</li>
+                </ul>) : null}
+                <p data-testid='value-element'>{value}</p>)
+                <h1>Hello from React</h1>
+                <button data-testid='toggle-btn' onClick={handleClick}>Create new contract</button>
+                <input onChange={handleChange} type="text" placeholder='Type here ...' />
+            </header>
+        </div>
+    );
 }
 
 export default App;
